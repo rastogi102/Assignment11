95cb0f83cfec1b72832c1d8d0e76b57f59eb14f4
rastogi102 <nikhilrastogi102@gmail.com>
2023-07-20 16:18:08 +0530

    first commit

---
+++ LargeDataCollection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConAppAssignment11
+{
+    public class LargeDataCollection<T> : IDisposable
+    {
+        private List<T> dataCollection;
+        private List<T> initialData;
+
+        public LargeDataCollection(IEnumerable<T> initialData)
+        {
+            this.initialData = new List<T>(initialData);
+            dataCollection = new List<T>(initialData);
+        }
+
+        public void Add(T item)
+        {
+            dataCollection.Add(item);
+        }
+
+        public bool Remove(T item)
+        {
+            return dataCollection.Remove(item);
+        }
+
+        public void PrintData()
+        {
+            foreach (T item in dataCollection)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Release managed resources
+                if (dataCollection != null)
+                {
+                    dataCollection.Clear();
+                    dataCollection = null;
+                }
+            }
+            // No need to release unmanaged resources in this case, as we only have managed resources.
+        }
+
+        public void PrintInitialData()
+        {
+            foreach (T item in initialData)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of LargeDataCollection
+            LargeDataCollection<int> dataCollection = new LargeDataCollection<int>(new List<int> { 10, 20, 30, 40, 50 });
+
+            // Print initial data
+            Console.Write("Initial data in the collection: ");
+            dataCollection.PrintInitialData();
+
+            // Adding elements to the collection
+            dataCollection.Add(60);
+            dataCollection.Add(70);
+
+            // Removing an element from the collection
+            bool removed = dataCollection.Remove(30);
+            Console.WriteLine("Item 30 is removed: " + removed);
+
+            // Accessing elements from the collection
+            Console.Write("Updated data in the collection: ");
+            dataCollection.PrintData();
+
+            // Dispose the LargeDataCollection object to release resources and free up memory
+            dataCollection.Dispose();
+
+        }
+    }
+}
+
+
